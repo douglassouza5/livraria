c2fbcdc52a9167097710794c4fc87ac9851da3df
Douglas de Oliveira Souza <douglas.nbsnet@gmail.com>
2017-05-31 17:43:23 -0300

    implementando model book

---
+++ src/main/java/br/com/livraria/managedbeans/AdminBooksBean.java
@@ -0,0 +1,11 @@
+package br.com.livraria.managedbeans;
+
+/**
+ * Created by desenvolvimento on 31/05/17.
+ */
+public class AdminBooksBean {
+
+    public void save(){
+        System.out.println("Salva livro!");
+    }
+}
